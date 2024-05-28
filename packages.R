43088cfed3fe7d78b9e952b2afcb989487462034
fnascimento-ucl <159649062+fnascimento-ucl@users.noreply.github.com>
2024-05-28 19:30:50 +0100

    Add files via upload

--- packages.R
+++ packages.R
@@ -1,118 +1,83 @@
-install.packages("shiny")
-install.packages("readxl")
-install.packages("dplyr")
-install.packages("magrittr")
-install.packages("ICC")
-install.packages("ggplot2")
-install.packages("reshape2")
-install.packages("cowplot")
-install.packages("gridExtra")
-install.packages("ggpp")
-install.packages("shinythemes")
-install.packages("shinyWidgets")
-install.packages("shinyBS")
-install.packages("shinydashboard")
-install.packages("dplyr")
-install.packages("shinydashboardPlus")
-install.packages("shinydisconnect")
-install.packages("reactlog")
-install.packages("plotly")
-install.packages("shinyjs")
-install.packages("shinycssloaders")
-install.packages("ggh4x")
-install.packages("colourpicker")
-install.packages("ggprism")
-install.packages("patchwork")
-install.packages("boot")
-install.packages("randomcoloR")
-install.packages("RColorBrewer")
-install.packages("colorspace")
-install.packages("shinydlplot")##not used?
-install.packages("openxlsx")
-install.packages("ggnewscale")
-install.packages("scales")
-install.packages("ggbreak")# not used yet (for scale breaks)
-install.packages("purrr")
-install.packages("ggExtra")
-install.packages("ggdist") # for raincloudplot
-install.packages("ggbeeswarm") # for raincloudplot
-install.packages("sjPlot") # for obtained HTML table from lmer
-install.packages("sjtable2df") # to convert HTML table
-install.packages("effectsize") # for lmer effect sizes
-install.packages("merDeriv") #for random effect confidence interval
-install.packages("knitr") #for LMM table
-install.packages("kableExtra") #for LMM table
-install.packages("MuMIn") #for getting R squared from LMM
-install.packages("datawizard")
-install.packages("berryFunctions") #to add empty rows
-#library(bslib) #for popovers and tooltips
-install.packages("extrafont")
-install.packages("stringr") # to trim variable name
-install.packages("shinybusy") # to display additional info if server is busy
-install.packages("shinyFeedback")
-install.packages("shinyToastify")
-install.packages("ggpubr")
-install.packages("shinyFiles")
-                 
+# library(shiny)
+# library(readxl)
+# library(dplyr)
+# library(magrittr)
+# library(ICC) # for intraclass coefficient calculation
+# library(ggplot2)
+# library(reshape2)
+# library(cowplot)
+# library(gridExtra)
+# library(ggpp)
+# library(DT)
+# source("functions/calculate_and_plot_icc.R")
+# library(shinythemes)
+# library(shinyWidgets)
+# library(shinyBS)
+# library(shinydashboard)
+# library(dplyr)
+# library(shinydashboardPlus)
+# library(shinydisconnect) #to allow disconnect message display
+# library(reactlog)
+# library(plotly)
+# library(shinyjs)
+# library(shinycssloaders)
+# library(ggh4x)#not used?
+# library(colourpicker)
+# library(ggprism)
+# library(patchwork)
+# library(boot)
+# library(randomcoloR)
+# library(RColorBrewer)
+# library(colorspace)
+# library(shinydlplot)##not used?
+# library(openxlsx)
+# library(ggnewscale)
+# library(scales)
+# library(ggbreak)# not used yet (for scale breaks)
+# library(purrr)
+# library(ggExtra)
+# library(ggdist) # for raincloudplot
+# library(ggbeeswarm) # for raincloudplot
+# library(sjPlot) # for obtained HTML table from lmer
+# library(sjtable2df) # to convert HTML table
+# library(effectsize) # for lmer effect sizes
+# library(merDeriv) #for random effect confidence interval
+# library(knitr) #for LMM table
+# library(kableExtra) #for LMM table
+# library(MuMIn) #for getting R squared from LMM
+# library(datawizard)
+# library(berryFunctions) #to add empty rows
+# #library(bslib) #for popovers and tooltips
+# library(extrafont)
+# library(stringr) # to trim variable name
+# library(shinybusy) # to display additional info if server is busy
+# library(shinyFeedback)
+# library(shinyToastify)
+# library(ggpubr)
+# library(shinyFiles)
 
-  
-library(shiny)
-library(readxl)
-library(dplyr)
-library(magrittr)
-library(ICC) # for intraclass coefficient calculation
-library(ggplot2)
-library(reshape2)
-library(cowplot)
-library(gridExtra)
-library(ggpp)
-library(DT)
-source("functions/calculate_and_plot_icc.R")
-library(shinythemes)
-library(shinyWidgets)
-library(shinyBS)
-library(shinydashboard)
-library(dplyr)
-library(shinydashboardPlus)
-library(shinydisconnect) #to allow disconnect message display
-library(reactlog)
-library(plotly)
-library(shinyjs)
-library(shinycssloaders)
-library(ggh4x)#not used?
-library(colourpicker)
-library(ggprism)
-library(patchwork)
-library(boot)
-library(randomcoloR)
-library(RColorBrewer)
-library(colorspace)
-library(shinydlplot)##not used?
-library(openxlsx)
-library(ggnewscale)
-library(scales)
-library(ggbreak)# not used yet (for scale breaks)
-library(purrr)
-library(ggExtra)
-library(ggdist) # for raincloudplot
-library(ggbeeswarm) # for raincloudplot
-library(sjPlot) # for obtained HTML table from lmer
-library(sjtable2df) # to convert HTML table
-library(effectsize) # for lmer effect sizes
-library(merDeriv) #for random effect confidence interval
-library(knitr) #for LMM table
-library(kableExtra) #for LMM table
-library(MuMIn) #for getting R squared from LMM
-library(datawizard)
-library(berryFunctions) #to add empty rows
-#library(bslib) #for popovers and tooltips
-library(extrafont)
-library(stringr) # to trim variable name
-library(shinybusy) # to display additional info if server is busy
-library(shinyFeedback)
-library(shinyToastify)
-library(ggpubr)
-library(shinyFiles)
+# installs and calls the install.load package as required
+if (!require(install.load)) {
+  install.packages("install.load")
+  library(install.load)
+}
+
+# list of packages required
+packages <- c(
+  "shiny", "readxl", "dplyr", "magrittr", "ICC", "ggplot2", "reshape2", 
+  "cowplot", "gridExtra", "ggpp", "DT", "shinythemes", "shinyWidgets", 
+  "shinyBS", "shinydashboard", "shinydashboardPlus", "shinydisconnect", 
+  "reactlog", "plotly", "shinyjs", "shinycssloaders", "ggh4x", "colourpicker", 
+  "ggprism", "patchwork", "boot", "randomcoloR", "RColorBrewer", "colorspace", 
+  "shinydlplot", "openxlsx", "ggnewscale", "scales", "ggbreak", "purrr", 
+  "ggExtra", "ggdist", "ggbeeswarm", "sjPlot", "sjtable2df", "effectsize", 
+  "merDeriv", "knitr", "kableExtra", "MuMIn", "datawizard", "berryFunctions", 
+  "extrafont", "stringr", "shinybusy", "shinyFeedback", "shinyToastify", 
+  "ggpubr", "shinyFiles"
+)
+
+# checks if packages are installed loads the packages
+install_load(packages)
 
 #UI modules
   #Data Selection and visualization
